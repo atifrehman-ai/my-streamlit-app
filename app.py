d78554344a965d44fda2c99d7ae2ecc9e406e00e
Atif Rehman <atifrehman.artificialintelligence@stmu.edu.pk>
2026-06-15 10:51:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import streamlit as st
 
-st.title("My First Streamlit App")
-st.write("Hello from Streamlit!")
+st.set_page_config(page_title="Smart Notes", page_icon="📝")
+
+st.title("📝 Smart Notes App")
+
+st.write("Create and save your notes.")
+
+note = st.text_area("Write your note here:")
+
+if st.button("Save Note"):
+    st.success("Note saved!")
+    st.write("Your Note:")
+    st.write(note)
